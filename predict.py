9fe34722cb3ffe0918e333a502ed0bd25faaaab4
Douglas Byers <douglasbyers@live.ca>
2023-01-13 15:00:49 -0500

    fixed a comment

--- predict.py
+++ predict.py
@@ -9,9 +9,11 @@ import cv2
 ap = argparse.ArgumentParser()
 ap.add_argument("-m", "--model", default="model", help="path to trained serialized model")
 ap.add_argument("-l", "--label-bin", default="binarizer", help="path to  label binarizer")
-ap.add_argument("-i", "--input", default="input.mp4", help="path to our input video") # change default to the filename of the input video/image
+ap.add_argument("-i", "--input", default="input.mp4", help="path to our input video")
+# [for above line] change default to the filename of the input video/image
 ap.add_argument("-o", "--output", default="output.avi", help="path to our output video")
-ap.add_argument("-s", "--size", type=int, default=128, help="size of queue for averaging") # change default to 1 if using a still image as input, use 128 if video (may need to decrease if very short video)
+ap.add_argument("-s", "--size", type=int, default=128, help="size of queue for averaging")
+# [for above line] change default to 1 if using a still image as input, use 128 if video (may need to decrease if very short video)
 args = vars(ap.parse_args())
 
 
